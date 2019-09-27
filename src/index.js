eba8f84fc56c578b2ebf35c0c180abcf9464ba9e
Antonio Fry <antoniofry2000@gmail.com>
2019-09-27 13:16:02 -0600

    setup index.js file for redux use

--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './Reducers';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
