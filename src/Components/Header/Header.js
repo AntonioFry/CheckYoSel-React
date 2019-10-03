83b282429546a3c65170f06ab2834d397f319a4d
Antonio Fry <antoniofry2000@gmail.com>
2019-10-02 18:35:43 -0600

    Style header components color and layout using flex

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -4,7 +4,9 @@ import './Header.css';
 export const Header = () => {
   return (
     <header>
-      <h1>CheckYoSelf</h1>
+      <div className="title-container">
+        <h1>CheckYoSelf</h1>
+      </div>
     </header>
   )
 };
