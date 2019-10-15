34e77e68043c51122cbb36d7fb5dba50d78b52dc
Antonio Fry <antoniofry2000@gmail.com>
2019-10-15 10:47:59 -0600

    Build out todos jsx

--- src/Components/Todos/Todos.js
+++ src/Components/Todos/Todos.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-const Todos = () => {
+const Todos = ({taskName, tasks}) => {
+  const formattedTasks = tasks.map(task => {
+    return (
+      <div>
+        <input type="checkbox" name={task}/>
+        <label for={task}>{task}</label>
+      </div>
+    )
+  })
   return (
     <article>
-      
+      <div>
+        <h3>{taskName}</h3>
+      </div>
+      <form>
+        {formattedTasks}
+      </form>
+      <div>
+
+      </div>
     </article>
   )
 }
