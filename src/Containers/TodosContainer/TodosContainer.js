6db0e9e44ea46471c1ac8c6ee21cc59a790668c7
Antonio Fry <antoniofry2000@gmail.com>
2019-10-15 11:04:08 -0600

    Style TodosContainer and Aside

--- src/Containers/TodosContainer/TodosContainer.js
+++ src/Containers/TodosContainer/TodosContainer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './TodosContainer.css'
 
 class TodosContainer extends Component {
 
@@ -7,7 +8,7 @@ class TodosContainer extends Component {
 
     
     return (
-      <section>
+      <section className="todos-section">
 
       </section>
     )
