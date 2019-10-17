a2541798580c2c6a8b8ba5a1c3ff2a7640c02263
Antonio Fry <antoniofry2000@gmail.com>
2019-10-17 11:22:52 -0600

    Successfully render TodoForm

--- src/Components/Aside/Aside.js
+++ src/Components/Aside/Aside.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import TodoForm from '../TodoForm/TodoForm';
 import './Aside.css';
 
 export const Aside = () => {
   return (
     <aside>
-      
+      <TodoForm />
     </aside>
   )
 }
--- src/Components/TodoForm/TodoForm.js
+++ src/Components/TodoForm/TodoForm.js
@@ -16,19 +16,19 @@ class TodoForm extends Component {
   render() {
     const { taskName, tasks } = this.state.todo
     return (
-      <form>
+      <form className="todo-form">
+        <label for={taskName}>Task Name</label>
         <input
           type="text"
           name={taskName}
           className="todo-form-input"
         />
-        <label for={taskName}>Task Name</label>
+        <label for={tasks}>Tasks</label>
         <input
           type="text"
           name={tasks}
           className="todo-form-input"
         />
-        <label for={tasks}>Tasks</label>
       </form>
     )
   }
