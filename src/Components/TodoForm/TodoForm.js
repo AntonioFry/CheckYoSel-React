6647cc0d6e6726f7b6bb822b39d64fc53fb4e3a3
Antonio Fry <antoniofry2000@gmail.com>
2019-10-17 11:05:52 -0600

    Initialize TodoForm component files

---
+++ src/Components/TodoForm/TodoForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './TodoForm.css';
+
+class TodoForm extends Component {
+  
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    <form>
+      
+    </form>
+  }
+}
+
+export default TodoForm;
