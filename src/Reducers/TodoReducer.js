ad9fd28ab334ba0bf22e8c9e918b4b5c7c681e8e
Antonio Fry <antoniofry2000@gmail.com>
2021-02-24 12:11:51 -0700

    Return the new todos list in todosReducer

--- src/Reducers/TodoReducer.js
+++ src/Reducers/TodoReducer.js
@@ -1,12 +1,13 @@
 const TodoReducer = (state = [], action) => {
   switch (action.type) {
     case "ADD_TODO":
-      return [...state, action.todo]
+      return [...state, action.todo];
 
     case "REMOVE_TODO":
       const newTodos = state.filter(todo => {
-        return todo.id !== action.todoId
-      })
+        return todo.id !== action.todoId;
+      });
+      return newTodos;
   
     default:
       return state;
