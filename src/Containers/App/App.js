e2c4382ffdf0f25b7df0b4942bd1fb4af0e5fa92
Antonio Fry <antoniofry2000@gmail.com>
2021-03-03 14:19:13 -0700

    Create method and property to toggle todos visiblity based on urgency

--- src/Containers/App/App.js
+++ src/Containers/App/App.js
@@ -5,13 +5,29 @@ import TodosContainer from '../TodosContainer/TodosContainer'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      urgentTodosVisibility: false,
+    }
+  }
+
+  changeTodosUrgentVisibility = (e) => {
+    e.preventDefault();
+    this.setState({ urgentTodosVisibility: !this.state.urgentTodosVisibility });
+    console.log(this.state.urgentTodosVisibility)
+  }
 
   render() {
     return (
       <main>
         <Header />
-        <Aside />
-        <TodosContainer />
+        <Aside
+          changeTodosUrgentVisibility={this.changeTodosUrgentVisibility}
+        />
+        <TodosContainer
+          urgentTodosVisibility={this.state.urgentTodosVisibility}
+        />
       </main>
     );
   }
